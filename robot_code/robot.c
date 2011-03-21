1a8910424615c72bf96defbc3c00705b8190d32f
chadklassen1@gmail.com <chadklassen1@gmail.com@71a0b24f-63ee-fc94-2ffb-8ce042bf7a32>
2011-03-21 23:42:06 +0000

    Beginnings of robot code

---
+++ robot_code/robot.c
@@ -0,0 +1,214 @@
+#define PROGRAM_TITLE "Assignment 1"
+#define DISPLAY_INFO "Klassen, Chad"
+
+#include <stdlib.h>  // Useful for the following includes.
+#include <stdio.h>    
+#include <string.h>  // For spring operations.
+
+#include <GL/gl.h>   // OpenGL itself.
+#include <GL/glu.h>  // GLU support library.
+#include <GL/glut.h> // GLUT support library.
+
+// Some global variables.
+// Window IDs, window width and height.
+int Window_ID;
+int Window_Width = 600;
+int Window_Height = 400;
+
+// Cube position and rotation speed variables.
+float X_Rot   = 0.5f;  // Initially only rotate around X-axix.
+float Y_Rot   = 0.0f;  // Later on you can use control keys to change
+float X_Speed = 0.0f;  // the rotation.
+float Y_Speed = 0.0f;
+float Z_Off   =-5.0f;
+
+
+
+/////////////////////////////////////////////////////////
+// Routine which actually does the drawing             //
+/////////////////////////////////////////////////////////
+void CallBackRenderScene(void)
+{
+  
+   
+   // Need to manipulate the ModelView matrix to move our model around.
+   glMatrixMode(GL_MODELVIEW);
+   
+   // Clear the color and depth buffers.
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+   // Move back to the origin
+   glLoadIdentity();
+   
+   gluLookAt(3.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+   GLUquadric* quad = gluNewQuadric();
+   glPushMatrix();
+   glScalef(0.75, 1.0, 0.75);
+   glutSolidCube(1);
+   glPopMatrix();
+   
+   glPushMatrix();
+   glTranslatef(0.0, 1.0, 0.0);
+   glRotatef(90, 1.0, 0.0, 0.0);
+   gluCylinder(quad, 0.25, 0.25, 0.5, 100, 100);
+   glPopMatrix();
+   
+   glPushMatrix();
+   glTranslatef(0.0, 1.4, 0.0);
+   glRotatef(90, 1.0, 0.0, 0.0);
+   glColor3f(0.2, 0.2, 0.5);
+   gluCylinder(quad, 0.1, 0.1, 0.2, 100, 100);
+   glPopMatrix();
+   
+   
+   glPushMatrix();
+   glTranslatef(-0.10, 1.1, 0.3);
+   glutSolidSphere(0.05, 15, 15);
+   glTranslatef(0.25, 0.0, 0.0);
+   glutSolidSphere(0.05, 15, 15);
+   glPopMatrix();
+   
+   glTranslatef(0.0, 1, 0.0);
+
+   glColor3f(0.2, 0.5, 0.5);
+
+   glutSolidCube(0.5);
+   
+ 
+   // All done drawing.  Let's show it.
+   glutSwapBuffers();
+
+   // Now let's do the motion calculations.
+   //X_Rot+=X_Speed; 
+   //Y_Rot+=Y_Speed;
+   X_Rot+=0.05f;
+   Y_Rot+=0.05f;
+}
+
+
+void myCBKey(unsigned char key, int x, int y)
+{
+   switch (key) {
+     case 97:
+      Z_Off -= 0.05f;
+     break;
+    case 122:
+      Z_Off += 0.05f;
+     break;
+    
+    }
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+// Callback routine executed whenever the window is resized. //
+//////////////////////////////////////////////////////////////
+void CallBackResizeScene(int Width, int Height)
+{
+   // Let's not core dump, no matter what.
+   if (Height == 0)
+      Height = 1;
+
+   glViewport(0, 0, Width, Height);
+
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
+
+   glMatrixMode(GL_MODELVIEW);
+
+   Window_Width  = Width;
+   Window_Height = Height;
+}
+
+////////////////////////////////////////////////////////
+//   Setup your program before passing the control    //
+//   to the main OpenGL event loop.                   //
+////////////////////////////////////////////////////////
+void MyInit(int Width, int Height) 
+{
+  
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+   GLfloat mat_shininess[] = { 50.0 };
+   GLfloat light_position[] = { 3.0, 3.0, 3.0, 0.0 };
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glShadeModel (GL_SMOOTH);
+
+   glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
+   
+   // Color to clear color buffer to.
+   glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+
+   // Depth to clear depth buffer to; type of test.
+   glClearDepth(1.0);
+   glDepthFunc(GL_LESS); 
+
+   // Enables Smooth Color Shading; try GL_FLAT for (lack of) fun.
+   glShadeModel(GL_SMOOTH);
+
+   // Load up the correct perspective matrix; using a callback directly.
+   CallBackResizeScene(Width,Height);
+}
+
+///////////////////////////////////////////////////
+// main() function.                              //
+//   There are general steps in making           //
+//   an OpenGL application.                      //
+//   Inits OpenGL.                               //
+//   Calls our own init function                 //
+//   then passes control onto OpenGL.            //   
+///////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+   glutInit(&argc, argv);
+   
+   // To see OpenGL drawing, take out the GLUT_DOUBLE request.
+   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+   // The following is for window creation.
+   // Set Window size
+   glutInitWindowSize(Window_Width, Window_Height);
+   // Create and Open a window with its title.
+   Window_ID = glutCreateWindow(PROGRAM_TITLE);
+
+   // Register and install the callback function to do the drawing. 
+   glutDisplayFunc(&CallBackRenderScene);
+
+   // If there's nothing to do, draw.
+   glutIdleFunc(&CallBackRenderScene);
+
+   // It's a good idea to know when our window's resized.
+   glutReshapeFunc(&CallBackResizeScene);
+
+   // Register and install the callback function for
+   // Some keys and special keys.
+   glutKeyboardFunc(&myCBKey);
+   
+
+   // OK, OpenGL's ready to go.  Let's call our own init function.
+   MyInit(Window_Width, Window_Height);
+
+   // Print out a bit of help dialog.
+   printf("\n%s\n\n", PROGRAM_TITLE);
+   printf("Print out some helpful information here.\n");
+   printf("When you program, you can also use\n");
+   printf("printf to do debugging.\n\n");
+
+   // Above functions represents those you will do to set up your
+   // application program.
+   // Now pass off control to OpenGL.
+   glutMainLoop(); 
+   // Never returns.
+   return 1; 
+}
+
+
